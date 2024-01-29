c4739c07d1c8c7491516c8de6c030c6c8bd7c171
eson1102 <duncanyu1102@gmail.com>
2024-01-29 17:18:44 +0800

    Update bilibili.js

--- musicfree/bilibili.js
+++ musicfree/bilibili.js
@@ -433,7 +433,7 @@ module.exports = {
     version: "0.1.12",
     author: "猫头猫",
     cacheControl: "no-cache",
-    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/bilibili/index.js",
+    srcUrl: "https://raw.githubusercontent.com/eson1102/rules/main/musicfree/bilibili.js",
     primaryKey: ["id", "aid", "bvid", "cid"],
     hints: {
         importMusicSheet: [
